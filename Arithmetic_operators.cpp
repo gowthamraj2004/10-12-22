f9f589ae2b5db0eea80c321ef87c5480a39d8a7d
gowthamraj2004 <gowthamrajsubbu2004@gmail.com>
2022-12-10 16:14:46 +0530

    first commit

---
+++ Arithmetic_operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    int x;
+    int y;
+    cin>>x;
+    cin>>y;
+    int sum = x + y;
+    int sub = x - y;
+    int mul = x * y;
+    int divi = x / y;
+    cout<<sum<<endl<<sub<<endl<<mul<<endl<<divi<<endl; 
+    cout<<"hello world";    
+ }
---
+++ LogicalAndOperator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x=4;
+    int y=4;
+    if ((x<=y)&&(x!=0))
+    cout<<"hello";
+    else
+    cout<<"oops";
+}
